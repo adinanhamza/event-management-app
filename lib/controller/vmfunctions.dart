efa2d908132c095d5585984d9bedb3afcd3aaeb9
adinanhamza <adinanck4557@gmail.com>
2025-03-01 15:49:47 +0530

    its changed to mvc architecture

---
+++ lib/controller/vmfunctions.dart
@@ -0,0 +1,36 @@
+
+import 'package:event_managment/model/vendormodel.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+
+
+
+  ValueNotifier<List<vendormodel>> vendorlistnotifier = ValueNotifier([]); 
+Future<void> togglefavorite(vendormodel vendor) async {
+  final vbox = await  Hive.openBox<vendormodel>('vendordatabase');
+  await vbox.add(vendor);
+
+  if (vendor.isfavorited == true) {
+    
+    await vendor.save(); 
+
+  }
+  getallfavoritevendors();
+}
+ Future <void> getallfavoritevendors()async {
+  final vbox = await Hive.openBox<vendormodel>('vendordatabase');
+  vendorlistnotifier.value.clear();
+  vendorlistnotifier.value.addAll(vbox.values);
+  vendorlistnotifier.notifyListeners();
+}
+
+Future<void> deletefavorite(vendormodel vendor,int index) async {
+  final vbox = await  Hive.openBox<vendormodel>('vendordatabase');
+  if (vendor.isfavorited == false) {
+    await vbox.deleteAt(index);
+  }
+  getallfavoritevendors();
+}
+
+
+
